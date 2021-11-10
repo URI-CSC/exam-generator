92435e3ac075fd84c29c6bab2d4e866d54c7fa63
Marco Alvarez <13228776+MarcoAlvarez@users.noreply.github.com>
2021-11-10 18:39:46 -0500

    major changes -- removed groups

--- autograder/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# install any software required (Ubuntu)
-#
-apt-get install -y python3 python3-pip
-pip3 install gradescope-utils
--- generator.py
+++ generator.py
@@ -36,22 +36,21 @@ class Generator:
     def next_question(self, ltx_fid, ans_fid):
         # shuffle questions in place
         random.shuffle(self.data)
+        q = self.data[0]
+        # print question header
+        ltx_fid.write('\n\n\\question[{}] {}\n'.format(q['points'], q['head'] if 'head' in q.keys() else ''))
         # print questions and delete used question from pool
-        if self.data[0]['type'] == 'tf':
-            self.print_tf(ltx_fid, ans_fid, self.data[0])
-            if len(self.data[0]['statements']) < self.data[0]['number']:
+        if q['type'] == 'tf':
+            self.print_tf(ltx_fid, ans_fid, q)
+            if len(q['statements']) < q['number']:
                 del self.data[0]
-        elif self.data[0]['type'] == 'single':
-            self.print_single(ltx_fid, ans_fid, self.data[0])
-            if self.n_questions != -1 and 'group' in self.data[0].keys():
-                self.remove_group(self.data[0]['group'])
-            else:
+        elif q['type'] == 'single':
+            self.print_single(ltx_fid, ans_fid, q)
+            if self.n_questions != -1 or len(q['question']) == 0:
                 del self.data[0]
-        elif self.data[0]['type'] == 'choices':
-            self.print_choices(ltx_fid, ans_fid, self.data[0])
-            if self.n_questions != -1 and 'group' in self.data[0].keys():
-                self.remove_group(self.data[0]['group'])
-            else:
+        elif q['type'] == 'choices':
+            self.print_choices(ltx_fid, ans_fid, q)
+            if self.n_questions != -1 or len(q['question']) == 0:
                 del self.data[0]
 
     def print_tf(self, ltx_fid, ans_fid, q):
@@ -59,44 +58,45 @@ class Generator:
         # shuffle statements in place
         random.shuffle(q['statements'])
         # print question
-        ltx_fid.write('\n\n\\question[{}] {}'.format(q['points'], q['question']))
+        ltx_fid.write(q['question'])
         ltx_fid.write('\\begin{itemize}')
         for i in range(q['number']):
-            ltx_fid.write('\t\\item[$({})$] {}'.format(2**i, q['statements'][i][1]))
+            ltx_fid.write('\n\t\\item[$({})$] {}'.format(2**i, q['statements'][i][1]))
             if q['statements'][i][0] == 'True':
                 answer += 2**i
         ltx_fid.write('\\end{itemize}')
-        ltx_fid.write('\\answerline')
+        ltx_fid.write('\n\\answerline')
         ans_fid.write('{}\n'.format(answer))
         # delete used statements
         del q['statements'][0:q['number']]
 
     def print_single(self, ltx_fid, ans_fid, q):
-        ltx_fid.write('\n\n\\question[{}] {}'.format(q['points'], q['question']))
-        ltx_fid.write('\\answerline')
-        ans_fid.write('{}\n'.format(q['answer']))
+        idx = random.randint(0, len(q['question'])-1)
+        ltx_fid.write(q['question'][idx])
+        ltx_fid.write('\n\\answerline')
+        ans_fid.write('{}\n'.format(q['answer'][idx]))
+        del q['question'][idx]
+        del q['answer'][idx]
 
     def print_choices(self, ltx_fid, ans_fid, q):
-        ltx_fid.write('\n\n\\question[{}] {}'.format(q['points'], q['question']))
-        ltx_fid.write('\\begin{choices}')
-        my_list = [ '\\CorrectChoice {}'.format(ans) for ans in q['correct'] ]
-        my_list +=  [ '\\choice {}'.format(ans) for ans in q['wrong'] ]        
+        idx = random.randint(0, len(q['question'])-1)
+        ltx_fid.write(q['question'][idx])
+        ltx_fid.write('\n\\begin{choices}')
+        my_list = []
+        if 'common' in q.keys():
+            my_list += [ '\n\t\\choice {}'.format(ans) for ans in q['common'] ]
+        my_list += ['\n\t\\choice {}'.format(q['answer'][idx])]
+        wrong = random.sample(range(len(q['random'])), 5 - len(my_list))
+        my_list += [ '\n\t\\choice {}'.format(q['random'][i]) for i in wrong ]
         random.shuffle(my_list)
-        correct = None
-        for i in range(len(my_list)):
-            if 'CorrectChoice' in my_list[i]:
-                correct = i
-            ltx_fid.write('\t{}'.format(my_list[i]))
-        ltx_fid.write('\\end{choices}')
-        ltx_fid.write('\\answerline')
-        ans_fid.write('{}\n'.format(chr(ord('A')+correct)))
-
-    def remove_group(self, g):
-        # find indices to be removed
-        idxs = [i for i in range(len(self.data)) if 'group' in self.data[i].keys() and g == self.data[i]['group']]
-        # delete elements
-        for idx in sorted(idxs, reverse = True):
-            del self.data[idx] 
+        [ltx_fid.write('\t{}'.format(ch)) for ch in my_list]
+        ltx_fid.write('\n\\end{choices}')
+        ltx_fid.write('\n\\answerline')
+        correct = [k for k, s in enumerate(my_list) if q['answer'][idx] in s]
+        assert(len(correct) == 1)
+        ans_fid.write('{}\n'.format(chr(ord('A')+correct[0])))
+        del q['question'][idx]
+        del q['answer'][idx]
 
 #####################################################################
 #####################################################################
@@ -105,8 +105,8 @@ if __name__ == "__main__":
     ## configure the command line options
     parser = ap.ArgumentParser(description='Generate a random exam in LaTeX format.')
     parser.add_argument('yaml_fname', help='File name for the question bank')
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-n', action='store', type=int, default=0, help='number of questions (default: 4)')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--n', action='store', type=int, default=4, help='number of questions (default: 4)')
     group.add_argument('--all', help='output ALL questions', action='store_true')
     parser.add_argument('--latex', metavar='<latex_fname>', action='store', help='File name for the LaTeX output', required=True)
     parser.add_argument('--output', metavar='<output_fname>', action='store', help='File name for the answers file', required=True)
