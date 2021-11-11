2d19e724c24117c928b937dd03f26ac3156d650c
Marco Alvarez <13228776+MarcoAlvarez@users.noreply.github.com>
2021-11-10 19:15:54 -0500

    major changes -- removed groups

--- example/gen-exams.sh
+++ example/gen-exams.sh
@@ -1,5 +1,5 @@
-python3 ../generator.py ../bank.yaml --n 4 --latex sample-exam-a.tex --output sample-exam-a.ans --preamble ../preamble.tex --seed 1
-python3 ../generator.py ../bank.yaml --n 4 --latex sample-exam-b.tex --output sample-exam-b.ans --preamble ../preamble.tex --seed 2
+python3 ../generator.py ../bank.yaml --n 5 --latex sample-exam-a.tex --output sample-exam-a.ans --preamble ../preamble.tex --seed 1
+python3 ../generator.py ../bank.yaml --n 5 --latex sample-exam-b.tex --output sample-exam-b.ans --preamble ../preamble.tex --seed 2
 pdflatex sample-exam-a.tex ; pdflatex sample-exam-a.tex ; pdflatex sample-exam-a.tex
 pdflatex sample-exam-b.tex ; pdflatex sample-exam-b.tex ; pdflatex sample-exam-b.tex
 rm *.aux *.log *.out
